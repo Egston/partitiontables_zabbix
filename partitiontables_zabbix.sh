b583451dd940ddf00e9ecd84545a627ea88aecdf
root <ilya.evseev@gmail.com>
2019-09-29 01:41:48 +0000

    Remove Chinese comments.

--- partitiontables_zabbix.sh
+++ partitiontables_zabbix.sh
@@ -6,7 +6,6 @@
 # funtion: create parition for zabbix MySQL 
 # repo: https://github.com/zabbix-book/partitiontables_zabbix
 
-#配置环境变量
 ZABBIX_CONF="/etc/zabbix/zabbix_server.conf"
 ZABBIX_USER="$(awk -F= '$1 == "DBUser"     { print $2; exit }' "$ZABBIX_CONF")"
  ZABBIX_PWD="$(awk -F= '$1 == "DBPassword" { print $2; exit }' "$ZABBIX_CONF")"
@@ -15,20 +14,16 @@ ZABBIX_PORT="$(awk -F= '$1 == "DBPort"     { print $2; exit }' "$ZABBIX_CONF")"
 ZABBIX_HOST="$(awk -F= '$1 == "DBHost"     { print $2; exit }' "$ZABBIX_CONF")"
 MYSQL_BIN="mysql"
 
-#历史数据保留时间，单位天
 HISTORY_DAYS=30
 
-#趋势数据保留时间，单位月
 TREND_MONTHS=12
 
 HISTORY_TABLE="history history_log history_str history_text history_uint"
 TREND_TABLE="trends trends_uint"
 
-#MYSQL连接命令
 MYSQL_CMD=$(echo ${MYSQL_BIN} -u${ZABBIX_USER:-zabbix} -p${ZABBIX_PWD} -P${ZABBIX_PORT:-3306} -h${ZABBIX_HOST:-127.0.0.1} ${ZABBIX_DB:-zabbix})
 
 function create_partitions_history() {
-    #给历史表创建分区
     for PARTITIONS_CREATE_EVERY_DAY in $(date +"%Y%m%d") $(date +"%Y%m%d" --date='1 days') $(date +"%Y%m%d" --date='2 days') $(date +"%Y%m%d" --date='3 days')  $(date +"%Y%m%d" --date='4 days') $(date +"%Y%m%d" --date='5 days') $(date +"%Y%m%d" --date='6 days') $(date +"%Y%m%d" --date='7 days')
     do
         TIME_PARTITIONS=$(date -d "$(echo ${PARTITIONS_CREATE_EVERY_DAY} 23:59:59)" +%s)
@@ -36,7 +31,6 @@ function create_partitions_history() {
         do
             SQL1=$(echo "show create table ${TABLE_NAME};")
             RET1=$(${MYSQL_CMD} -e "${SQL1}"|grep "PARTITION BY RANGE"|wc -l)
-            #表结构中的表分区不存在，则创建
             if [ "${RET1}" == "0" ];then
                 SQL2=$(echo "ALTER TABLE $TABLE_NAME PARTITION BY RANGE( clock ) (PARTITION p${PARTITIONS_CREATE_EVERY_DAY}  VALUES LESS THAN (${TIME_PARTITIONS}));")
                 RET2=$(${MYSQL_CMD} -e "${SQL2}")
@@ -48,7 +42,6 @@ function create_partitions_history() {
                 fi
                 continue
             fi
-            #表结构中的表分区已经存在，则创建分区
             if [ "${RET1}" != "0" ];then
                 SQL3=$(echo "show create table ${TABLE_NAME};")
                 RET3=$(${MYSQL_CMD} -e "${SQL3}"|grep "p${PARTITIONS_CREATE_EVERY_DAY}"|wc -l)
@@ -69,7 +62,6 @@ function create_partitions_history() {
 }
 
 function drop_partitions_history() {
-    #删除历史表分区
     for PARTITIONS_DELETE_DAYS_AGO in $(date +"%Y%m%d" --date="${HISTORY_DAYS} days ago")
     do
         for TABLE_NAME in ${HISTORY_TABLE}
@@ -91,7 +83,6 @@ function drop_partitions_history() {
 }
 
 function create_partitions_trend() {
-    #创建趋势表分区
     for PARTITIONS_CREATE_EVERY_MONTHS in $(date +"%Y%m") $(date +"%Y%m" --date='1 months') $(date +"%Y%m" --date='2 months') $(date +"%Y%m" --date='3 months') $(date +"%Y%m" --date='4 months') $(date +"%Y%m" --date='5 months')
     do
         TIME_PARTITIONS=$(date -d "$(echo ${PARTITIONS_CREATE_EVERY_MONTHS}01 00:00:00)" +%s)
@@ -99,7 +90,6 @@ function create_partitions_trend() {
         do
             SQL1=$(echo "show create table ${TABLE_NAME};")
             RET1=$(${MYSQL_CMD} -e "${SQL1}"|grep "PARTITION BY RANGE"|wc -l)
-            #表结构中的表分区不存在，则创建
             if [ "${RET1}" == "0" ];then
                 SQL2=$(echo "ALTER TABLE $TABLE_NAME PARTITION BY RANGE( clock ) (PARTITION p${PARTITIONS_CREATE_EVERY_MONTHS}  VALUES LESS THAN (${TIME_PARTITIONS}));")
                 RET2=$(${MYSQL_CMD} -e "${SQL2}")
@@ -111,12 +101,10 @@ function create_partitions_trend() {
                 fi
                 continue
             fi
-            #表结构中的表分区已经存在，则创建分区
             if [ "${RET1}" != "0" ];then
                 SQL3=$(echo "show create table ${TABLE_NAME};")
                 RET3=$(${MYSQL_CMD} -e "${SQL3}"|grep "p${PARTITIONS_CREATE_EVERY_MONTHS}"|wc -l)
                 if [ "${RET3}" == "0" ];then
-                     
                     SQL4=$(echo "ALTER TABLE ${TABLE_NAME}  ADD PARTITION (PARTITION p${PARTITIONS_CREATE_EVERY_MONTHS} VALUES LESS THAN (${TIME_PARTITIONS}));")
                     RET4=$(${MYSQL_CMD} -e "${SQL4}")
                     if [ "${RET4}" != "" ];then
@@ -132,7 +120,6 @@ function create_partitions_trend() {
 }
 
 function drop_partitions_trend() {
-    #删除趋势表分区
     for PARTITIONS_DELETE_MONTHS_AGO in $(date +"%Y%m" --date="${TREND_MONTHS} months ago")
     do
         for TABLE_NAME in ${TREND_TABLE}
